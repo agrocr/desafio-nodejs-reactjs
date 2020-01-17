67b649761cc698d87e9f1c66d04ef8f8d15560ca
Gabriel Rodrigues Souza <gabriel.souza@cotriguacu.com.br>
2020-01-17 13:16:28 -0300

    add update page

---
+++ frontend/src/pages/updatePerson/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class UpdatePerson extends Component {
+  render() {
+    return <h1>Update Person</h1>;
+  }
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Main from "./pages/main";
 import Person from "./pages/person";
 import CreatePerson from "./pages/createPerson";
+import UpdatePerson from "./pages/updatePerson";
 
 const Routes = () => (
   <BrowserRouter>
@@ -12,6 +13,7 @@ const Routes = () => (
       <Route exact path="/" component={Main} />
       <Route path="/person/:id" component={Person} />
       <Route path="/create/person" component={CreatePerson} />
+      <Route path="/update/person" component={UpdatePerson} />
     </Switch>
   </BrowserRouter>
 );

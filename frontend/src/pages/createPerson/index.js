bb3f4d8efe6991b73fede5fcab26a7249e8885c0
Gabriel Rodrigues Souza <gabriel.souza@cotriguacu.com.br>
2020-01-17 17:23:44 -0300

    Person page form styles

--- frontend/src/pages/createPerson/index.js
+++ frontend/src/pages/createPerson/index.js
@@ -1,7 +1,56 @@
 import React, { Component } from "react";
 
+import "./styles.css";
+
 export default class CreatePerson extends Component {
   render() {
-    return <h1>Create Person</h1>;
+    return (
+      <div id="app">
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="nome">Nome:</label>
+            <input name="nome" id="nome" required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="cpf">CPF:</label>
+            <input name="cpf" id="cpf" required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="idade">Idade:</label>
+            <input name="idade" id="idade" required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="sexo">Sexo:</label>
+            <input name="sexo" id="sexo" required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="telefone">Telefone:</label>
+            <input name="telefone" id="telefone" required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="email">Email:</label>
+            <input name="email" id="email" required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="nome">Status:</label>
+            <select name="ativo" id="ativo">
+              <option value="true" selected>ATIVO</option>
+              <option value="false">INATIVO</option>
+            </select>
+          </div>
+          <div className="button-group">            
+            <button type="button">Voltar</button>
+            <button type="submit">Salvar</button>
+          </div>
+        </form>
+      </div>
+    )
   }
 }
